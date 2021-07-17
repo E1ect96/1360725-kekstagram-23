17e6810d0f01948b31121b92364d77b5a391357a
e1ect <e1ectt96@gmail.com>
2021-07-17 04:43:04 +0300

    вывод дополнительных комментариев 1 раз

--- js/render-fullview.js
+++ js/render-fullview.js
@@ -3,7 +3,7 @@ import {isEscEvent} from './utils.js';
 
 const closeButton = popup.querySelector('#picture-cancel');
 const comentsCounter = popup.querySelector('.social__comment-count');
-/*const comentsLoader = popup.querySelector('.comments-loader');*/
+const comentsLoader = popup.querySelector('.comments-loader');
 
 
 const onEscPress = function (evt) {
@@ -18,39 +18,48 @@ const renderFullview = function (picturesData, photoId) {
   popup.querySelector('img').src = currentPhoto.url;
   popup.querySelector('.social__caption').textContent = currentPhoto.description;
   popup.querySelector('.likes-count').textContent = currentPhoto.likes;
-  popup.querySelector('.comments-count').textContent = currentPhoto.comments.length;
 
-  const COMMENTS_COUNT = 5;
-  comentsCounter.textContent = COMMENTS_COUNT;
+  let COMMENTS_COUNT = 5;
+  let START_INDEX = 0;
   const commentsBlock = popup.querySelector('.social__comments');
   commentsBlock.textContent = '';
   const commentsFragment = new DocumentFragment();
 
-  for (let i = 0; i < COMMENTS_COUNT; i++) {
-    /*currentPhoto.comments.forEach((comment) =>*/
-    const commentItem = document.createElement('li');
-    commentItem.classList.add('social__comment');
-    const commentAvatar = document.createElement('img');
-    commentAvatar.classList.add('social__picture');
-    commentAvatar.src = currentPhoto.comments[i].avatar;
-    commentAvatar.alt = currentPhoto.comments[i].name;
-    commentAvatar.width = 35;
-    commentAvatar.height = 35;
+  const showMoreComments = function () {
+    if (currentPhoto.comments.length - COMMENTS_COUNT < 0) {
+      COMMENTS_COUNT = currentPhoto.comments.length;
+    }
+    if (COMMENTS_COUNT <= currentPhoto.comments.length) {
+      for (let i = START_INDEX; i < COMMENTS_COUNT; i++) {
+        const commentItem = document.createElement('li');
+        commentItem.classList.add('social__comment');
+        const commentAvatar = document.createElement('img');
+        commentAvatar.classList.add('social__picture');
+        commentAvatar.src = currentPhoto.comments[i].avatar;
+        commentAvatar.alt = currentPhoto.comments[i].name;
+        commentAvatar.width = 35;
+        commentAvatar.height = 35;
+        const commentText = document.createElement('p');
+        commentText.classList.add('social__text');
+        commentText.textContent = currentPhoto.comments[i].message;
 
-    const commentText = document.createElement('p');
-    commentText.classList.add('social__text');
-    commentText.textContent = currentPhoto.comments[i].message;
-
-    commentItem.appendChild(commentAvatar);
-    commentItem.appendChild(commentText);
-    commentsFragment.appendChild(commentItem);
-  }
-
-  commentsBlock.appendChild(commentsFragment);
-
+        commentItem.appendChild(commentAvatar);
+        commentItem.appendChild(commentText);
+        commentsFragment.appendChild(commentItem);
+        commentsBlock.appendChild(commentsFragment);
+        comentsCounter.textContent = `${COMMENTS_COUNT  } из ${  currentPhoto.comments.length  } комментариев`;
+      }
+      START_INDEX += 5;
+      COMMENTS_COUNT += 5;
+      /*console.log(START_INDEX);
+      console.log(COMMENTS_COUNT);*/
+    }
+  };
+  showMoreComments();
+  comentsLoader.addEventListener('click', showMoreComments);
   document.addEventListener('keydown', onEscPress);
   popupOpen();
   closeButton.addEventListener('click', popupClose);
 };
 
-export {renderFullview, closeButton, onEscPress};
+export {renderFullview, closeButton, onEscPress, comentsLoader};
